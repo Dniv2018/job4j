f64f10edac147f558236608ca3c7484b68a1d45c
Dniv2018 <nikolaevdmitry77@gmail.com>
2018-11-01 11:51:56 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,14 @@
 package ru.job4j.calculate;
+
+/**
+* Вывод строки в консоль.
+* @author Petr Arsentev (parsentev@yandex.ru)
+*/
 public class Calculate {
+	/**
+	* Main
+	* @param arg - arg.
+	*/
 	public static void main (String[] arg) {
 		System.out.println ("Hello World");
 	}
