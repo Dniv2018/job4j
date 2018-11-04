d107d0c6ebbacddd07f8aabff4f49ccdf671a35a
Dniv2018 <nikolaevdmitry77@gmail.com>
2018-11-04 13:01:43 +0300

    2.5. Инспектор кода IDEA. [#69757]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,24 +1,24 @@
 package ru.job4j.calculate;
 
 /**
-* Вывод строки в консоль.
-* @author Petr Arsentev (parsentev@yandex.ru)
-* @since 2018-10-31
-*/
+ * Вывод строки в консоль.
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @since 2018-10-31
+ */
 public class Calculate {
 	/**
-	* Main
-	* @param arg - arg.
-	*/
+	 * Main
+	 * @param arg - arg.
+	 */
 	public static void main(String[] arg) {
 		System.out.println("Hello World");
 	}
-	
+
 	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
+	 * Method echo.
+	 * @param name Your name.
+	 * @return Echo plus your name.
+	 */
 	public String echo(String name) {
 		return "Echo, echo, echo : " + name;
 	}
