3aae854d0995a7d66bb7f9682352ca6b118bb6e6
Dniv2018 <nikolaevdmitry77@gmail.com>
2019-03-13 11:15:07 +0300

    4.4. Максимум из трех чисел [#190]

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -2,10 +2,20 @@ package ru.job4j.loop;
 
 public class Factorial {
     public int calc(int n) {
-        int result = 0;
-        for (int i=0; i<=n; i++){
-            result = i==0 ? 1: result*i;
-            }
-        return result;
+        int result = 1;
+//        result++;
+        switch (n) {
+            case 0:
+                return result;
+            default:
+//                result++;
+                for (int i = 1; i <= n; i++) {
+                    result = result * i;
+
+                }
+//            result = i==0 ? 1: result*i;
+//            }
+                return result;
+        }
     }
 }
