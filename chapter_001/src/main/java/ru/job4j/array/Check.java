615ba1f39a00f748afcc48fb79ca74a7bd6ffb63
Dniv2018 <nikolaevdmitry77@gmail.com>
2019-06-12 17:32:42 +0300

    6.3. Массив заполнен true или false

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+/**
+ * Проверка boolean-массива на заполненость только true или только false.
+ * @author Dmitry Nikolaev (dymy@yandex.ru)
+ * @since 2019-06-12
+ */
+public class Check {
+    public boolean mono(boolean[] data) {
+        boolean broker = data[data.length-1];
+        for (boolean i: data)
+            if (i^broker) return false;
+        return true;
+    }
+}
