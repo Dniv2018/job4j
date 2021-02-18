9d5f85d5b357feb5526d5bca3328fcde85711fbc
Dniv2018 <NikolaevDmitry77@gmail.com>
2021-02-18 10:56:15 +0300

    2. Перегрузка конструктора

---
+++ chapter_002/src/main/java/ru/job4j/tracker/oop/Item.java
@@ -0,0 +1,37 @@
+package ru.job4j.tracker.oop;
+
+public class Item {
+    private String name;
+    int id;
+
+    public Item() {
+    }
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        //Item item = new Item();
+        Item itemVoid = new Item();
+        System.out.println(itemVoid);
+
+        Item itemId = new Item (9);
+        System.out.println(itemId.id);
+
+        Item itemNameId = new Item ("Pasha", 10);
+        System.out.println(itemNameId.name + " " + itemNameId.id);
+
+        Item itemName = new Item ("Mary");
+        System.out.println(itemName.name);
+    }
+}
